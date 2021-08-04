61ffbf03ef8905b657e4a390330fe671e2d709a9
NileshPhadtareMedsolis <n.phadtare@medsolis.in>
2021-08-04 15:21:38 +0530

    Initial Commit

---
+++ Shared/DemoLikertScaleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLikertScaleAppApp.swift
+//  Shared
+//
+//  Created by Nilesh Phadtare on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoLikertScaleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
